3023f69fdef22518f6b13013a93c97d8f736d5ea
duane-faria <duane.faria@etec.sp.gov.br>
2020-09-19 15:56:21 -0300

    Criando css com primeiras estilizações

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <nav className='container'>
+      <Link to='/'>Home</Link>
+      <Link to='/login'>Login / criar login</Link>
+    </nav>
+  );
 }
