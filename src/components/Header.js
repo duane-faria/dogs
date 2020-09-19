6d7a196040bb9aeec320c55da12b8acf433e7d31
duane-faria <duane.faria@etec.sp.gov.br>
2020-09-19 16:30:36 -0300

    Criando o header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
+// para usar um svg importamos ele como um componente
 
 export default function Header() {
   return (
-    <nav className='container'>
-      <Link to='/'>Home</Link>
-      <Link to='/login'>Login / criar login</Link>
-    </nav>
+    <div className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to='/' arial-label='Dogs - home' className={styles.logo}>
+          <Dogs />
+        </Link>
+        <Link to='/login' className={styles.login}>
+          Login / criar login
+        </Link>
+      </nav>
+    </div>
   );
 }
