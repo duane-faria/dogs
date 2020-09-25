b4100357e7520428f342a051095cad3f30f48f0b
duane-faria <duane.faria@etec.sp.gov.br>
2020-09-25 17:38:09 -0300

    Estilizanod

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -6,6 +6,9 @@ import useForm from '../../Hooks/useForm';
 import { TOKEN_POST, USER_GET } from '../../api';
 import TokenPost from '../../services/endpoints/TokenPost';
 import { UserContext } from '../../UserContext';
+import Error from '../helper/Error';
+import styles from './LoginForm.module.css';
+import styleBtn from '../forms/Button.module.css';
 
 export default function LoginForm() {
   const username = useForm();
@@ -24,9 +27,9 @@ export default function LoginForm() {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+    <section className='animeLeft'>
+      <h1 className='title'>Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label='Usuário' type='text' name='username' {...username} />
         <Input label='Senha' type='password' name='password' {...password} />
 
@@ -35,9 +38,18 @@ export default function LoginForm() {
         ) : (
           <Button>Entrar</Button>
         )}
-        {error && <p>{error}</p>}
+        {error && <Error error={error} />}
       </form>
-      <Link to='/login/criar'>Cadastro</Link>
+      <Link className={styles.lost__password} to='/login/perdeu'>
+        Perdeu a Senha?
+      </Link>
+      <div className={styles.register}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={styleBtn.button} to='/login/criar'>
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 }
