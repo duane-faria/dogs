7c564217d5ff56d2c2cc80a2dc41344a9ccf9953
duane-faria <duane.faria@etec.sp.gov.br>
2020-10-13 12:22:14 -0300

    Adicionando biblioteca de gráficos

--- src/components/user/UserStats.js
+++ src/components/user/UserStats.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import Head from '../helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../api';
+import Loading from '../helper/Loading';
+import Error from '../helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 export default function UserStats() {
-  return (
-    <div>
-      <Head title='Estatísticas' />
-      UserStats
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title='Estatísticas' />
+        <UserStatsGraphs data={data}/>
+      </div>
+    );
+  else return null;
 }
